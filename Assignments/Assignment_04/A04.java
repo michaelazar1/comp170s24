d2823f68429545531268febccafbd84714c76521
michaelazar1 <mazar2@luc.edu>
2024-06-26 12:24:34 -0500

    Update A04.java

--- Assignments/Assignment_04/A04.java
+++ Assignments/Assignment_04/A04.java
@@ -4,5 +4,17 @@ public class A04 {
         printEvenCharacters("Springfield"); // Expect: S r n f e d 
     } // method main
 
+    public static void printOddCharacters(String string) {
+        for (int i = 1; i < string.length(); 1 += 2) {
+            System.out.print(string.charAt(i) + " ");
+        }
+        System.out.println();
+    }
 
+    public static void printEvenCharacters(string string) {
+        for (int i = 0; i < string.length(); i += 2) { 
+            System.out.print(string.charAt(i) = " "); 
+        }
+        System.out.println();
+    }
 }
