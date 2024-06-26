427b58420c6f81b8a37ca776cd701fd583b0ba05
michaelazar1 <mazar2@luc.edu>
2024-06-26 21:21:33 +0000

    A04 Homework Save

--- Assignments/Assignment_04/A04.java
+++ Assignments/Assignment_04/A04.java
@@ -4,5 +4,19 @@ public class A04 {
         printEvenCharacters("Springfield"); // Expect: S r n f e d 
     } // method main
 
+    /* 2nd character, 4th character, 6th character, 8th character, and 10th character in word are considered odd since we count from 0 */
+    public static void printOddCharacters(String string) {
+        for (int i = 1; i < string.length(); i += 2) {
+            System.out.print(string.charAt(i) + " ");
+        }
+        System.out.println();
+    }
 
+    /* 1st character, 3rd character, 5th character, 7th character, 9th character, and 11th character in word are considered even since we count from 0 */
+    public static void printEvenCharacters(String string) {
+        for (int i = 0; i < string.length(); i += 2) { 
+            System.out.print(string.charAt(i) + " "); 
+        }
+        System.out.println();
+    }
 }
